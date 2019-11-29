408b6f986317796eabe4d7d2a622d1f3fbf89d9a
jaroslaw <jarecki1988@gmail.com>
2019-11-29 21:59:09 +0100

    random kv counter

--- src/Components/status/status.js
+++ src/Components/status/status.js
@@ -3,8 +3,9 @@ import falcon from "../../../src/falcon2.png";
 import Lightsaber from "../../../src/lightsaber2.png";
 
 
-let random = Math.floor(Math.random()*7);
-let value = random;
+let random1 = Math.floor(Math.random()*7);
+let random2 = Math.floor(Math.random()*7);
+let value = random1 + random2;
 
 
 
@@ -14,9 +15,16 @@ const Status = () => {
     <div className="status">
      <h1>TOTAL POWER:{value} KV </h1>
      <div className="spacer">
+       <figure>
      <img className="falcon" src={falcon} alt="falcon" ></img>
+     <figurecaption>{random1} KV </figurecaption>
+     </figure>
+     <figure>
      <img className="light" src={Lightsaber} alt="falcon" ></img>
+     <figurecaption>{random2} KV </figurecaption>
+     </figure>
      </div>
+     
      <footer>Copyright all reserved 2019</footer>
     </div>
   );
