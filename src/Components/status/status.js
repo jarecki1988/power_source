a243ace883ef2b3f29b68cb55e2675832c3bf3b2
jaroslaw <jarecki1988@gmail.com>
2019-11-30 00:51:29 +0100

    update

--- src/Components/status/status.js
+++ src/Components/status/status.js
@@ -13,19 +13,18 @@ let value = random1 + random2;
 const Status = () => {
   return (
     <div className="status">
-     <h1>TOTAL POWER:{value} KV </h1>
-     <div className="spacer">
-       <figure>
-     <img className="falcon" src={falcon} alt="falcon" ></img>
-     <figurecaption>{random1} KV </figurecaption>
-     </figure>
-     <figure>
-     <img className="light" src={Lightsaber} alt="falcon" ></img>
-     <figurecaption>{random2} KV </figurecaption>
-     </figure>
-     </div>
-     
-     <footer>Copyright all reserved 2019</footer>
+      <h1>TOTAL POWER:{value} KV </h1>
+        <div className="space">
+          <figure className="figure">
+            <figurecaption>{random1} KV </figurecaption>
+              <img className="falcon" src={falcon} alt="falcon" ></img>
+              <img className="light" src={Lightsaber} alt="falcon" ></img>
+            <figurecaption>{random2} KV </figurecaption>
+          </figure>
+        </div>
+      <footer>
+        <h5>Copyright all reserved 2019</h5>
+      </footer>
     </div>
   );
 }
