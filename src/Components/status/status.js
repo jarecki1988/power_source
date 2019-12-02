4129a7fe47960fec39741a3a43307dacd08f0857
jaroslaw <jarecki1988@gmail.com>
2019-12-02 15:10:14 +0100

    add working onClick number generator

--- src/Components/status/status.js
+++ src/Components/status/status.js
@@ -12,7 +12,7 @@ class Status extends React.Component {
   constructor(props){
     super(props);
     this.state = {border: '2px solid gray'};
-    this.state = {random: 0};
+    this.state = {random: 'KV'}
     this.handleClick = this.handleClick.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.randomNum = this.randomNum.bind(this);
@@ -23,28 +23,27 @@ class Status extends React.Component {
 
     this.setState(state => 
       ({border: '2px solid green'})) 
-      this.state = alert('Kliknięto w falcon!');
+      alert ('Kliknięto w falcon!');
   }
   handleClick2() {
 
     this.setState(state => 
-      ({border: '2px solid green'}))
-      this.state = alert('Kliknięto w lightsword!');
+      ({border: '2px solid lightgreen'}))
+      alert('Kliknięto w lightsword!');
       
   }
   randomNum() {
-    this.setState({
-      randomGen = Math.floor(Math.random()*7),
-      this.state = randomGen
-    })
-  }
+    this.setState(state => 
+      this.random = (Math.random()*700/100).toFixed(1))
+    }
+  
 
   render() {
   return (
     <div className="status">
       <h1>TOTAL POWER:{} KV </h1>
         <div className="status__space">
-        <h6><output>{this.state.random}</output></h6>
+        <h6><output>{[this.random,this.state.random]} </output></h6>
                <img className="status__falcon" src={falcon} alt="falcon" style={{border: this.state.border}} onClick={this.randomNum} ></img>
               <img className="status__light" src={Lightsaber} alt="falcon" style={{border: this.state.border}} onClick={this.handleClick2}></img>
             <h6><Random/> KV </h6>
