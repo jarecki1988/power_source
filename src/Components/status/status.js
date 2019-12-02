e39bd340cc240d80d027242d69edd58e7b248a24
jaroslaw <jarecki1988@gmail.com>
2019-12-02 17:23:20 +0100

    add media queries

--- src/Components/status/status.js
+++ src/Components/status/status.js
@@ -3,33 +3,25 @@ import falcon from "../../Assets/Images/falcon2.png";
 import Lightsaber from "../../Assets/Images/lightsaber2.png";
 import Random from '../Random/random'
 
-
-
-
-
-
 class Status extends React.Component {
   constructor(props){
     super(props);
     this.state = {border: '2px solid gray'};
-    this.state = {random: 'KV'}
+    this.state = {random: '0KV'}
     this.handleClick = this.handleClick.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.randomNum = this.randomNum.bind(this);
   }
 
-
- handleClick() {
-
+  handleClick() {
     this.setState(state => 
       ({border: '2px solid green'})) 
-      alert ('Kliknięto w falcon!');
+        alert ('Kliknięto w falcon!');
   }
   handleClick2() {
-
     this.setState(state => 
       ({border: '2px solid lightgreen'}))
-      alert('Kliknięto w lightsword!');
+        alert('Kliknięto w lightsword!');
       
   }
   randomNum() {
@@ -37,22 +29,27 @@ class Status extends React.Component {
       this.random = (Math.random()*700/100).toFixed(1))
     }
   
-
   render() {
-  return (
-    <div className="status">
-      <h1>TOTAL POWER:{} KV </h1>
-        <div className="status__space">
-        <h6><output>{[this.random,this.state.random]} </output></h6>
-               <img className="status__falcon" src={falcon} alt="falcon" style={{border: this.state.border}} onClick={this.randomNum} ></img>
+    return (
+      <div className="status">
+        <h1>TOTAL POWER:{} KV </h1>
+          <div className="status__space">
+              <output >{[this.random,this.state.random]}</output>
+              <img className="status__falcon" src={falcon} alt="falcon"  style={{border: this.state.border}} onClick={this.randomNum} ></img>
+              
               <img className="status__light" src={Lightsaber} alt="falcon" style={{border: this.state.border}} onClick={this.handleClick2}></img>
-            <h6><Random/> KV </h6>
-        </div>
-      <footer>
-        Copyright all reserved 2019.
-      </footer>
-    </div>
-  );
+              <output >{[this.random,this.state.random]}</output>
+              
+              
+          </div>
+
+          <footer>
+            Copyright all reserved 2019.
+          </footer>
+
+        </div>  
+    );
+  }
 }
-}
+
 export default Status;
